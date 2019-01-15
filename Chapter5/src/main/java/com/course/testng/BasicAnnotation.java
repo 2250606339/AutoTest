f3776bc9dd875b4f1ded9ae1dc4e97c2f20e42ce
mavis <2250606339@qq.com>
2019-01-15 18:25:16 +0800

    提交Chapter5——BeforeMethod和AfterMethod

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,7 +1,7 @@
 package com.course.testng;
 
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
 
@@ -11,4 +11,20 @@ public class BasicAnnotation {
         System.out.println("这是测试用例1");
 
     }
+
+    @Test
+    public void testCase2(){
+        System.out.println("这是测试用例2");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("BeforeMethod这是在测试方法之前运行的");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("AfterMethod这是在测试方法之后运行的");
+    }
+
 }
